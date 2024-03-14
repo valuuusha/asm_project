1db8357b9742861d3128f53bb96f98e9004c2f50
valuuusha <162372335+valuuusha@users.noreply.github.com>
2024-03-14 02:18:14 +0200

    Added method for binary conversion

--- ImplementCode.java
+++ ImplementCode.java
@@ -5,9 +5,35 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ImplementCode {
-    public static void main(String[] args) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("file.txt"))){
+    public static String toBinary(int num) {
+        if (num == 0) return "0";
+
+        StringBuilder binary = new StringBuilder();
+        boolean isNegative = false;
+
+        if (num < 0) {
+            isNegative = true;
+            num = -num;
+        }
+
+        while (num > 0) {
+            binary.insert(0, num % 2);
+            num /= 2;
+        }
+
+        if (isNegative) {
+            while (binary.length() < 32) {
+                binary.insert(0, '0');
+            }
+        }
+
+        return binary.toString();
+    }
+
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Vale\\AppData\\Roaming\\JetBrains\\IdeaIC2023.2\\scratches\\file.txt"))){
             String line;
             int totalNumbers = 0; // Лічильник загальної кількості чисел
 
@@ -20,13 +46,17 @@ public class ImplementCode {
                 totalNumbers += numbers.length;
 
                 if (totalNumbers > 10000) {
-                    System.out.println("INvalid input");
+                    System.out.println("Invalid input");
                     return;
                 }
 
+                for (String numberStr : numbers) {
+                    int number = Integer.parseInt(numberStr);
+                    toBinary(number);
+                }
             }
         } catch (IOException e) {
-            e.fillInStackTrace();
+            e.printStackTrace();
         }
     }
 }
