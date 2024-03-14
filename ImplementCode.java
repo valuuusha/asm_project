1e33c7f136677b13610a2bca7c7ee1d6deb295ee
valuuusha <162372335+valuuusha@users.noreply.github.com>
2024-03-14 08:18:15 +0200

    Added method for bubble sort

--- ImplementCode.java
+++ ImplementCode.java
@@ -30,6 +30,18 @@ public class ImplementCode {
 
         return binary.toString();
     }
+    public static void bubbleSort(String[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    String temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -50,9 +62,17 @@ public class ImplementCode {
                     return;
                 }
 
-                for (String numberStr : numbers) {
-                    int number = Integer.parseInt(numberStr);
-                    toBinary(number);
+                String[] binaryNumbers = new String[numbers.length];
+
+                for (int i = 0; i < numbers.length; i++) {
+                    int number = Integer.parseInt(numbers[i]);
+                    binaryNumbers[i] = toBinary(number);
+                }
+
+                bubbleSort(binaryNumbers);
+
+                for (String binaryNumber : binaryNumbers) {
+                    System.out.println(binaryNumber);
                 }
             }
         } catch (IOException e) {
